7c102e03f9eb592812b061d31666c3c272602d08
Danil Deineka <tripledpadik@gmail.com>
2023-03-09 23:03:33 +0200

    make task 8 and 9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const formEl = document.querySelector(".login-form");
+
+formEl.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const {email, password}= event.currentTarget;
+
+
+    if(email.value.trim() === "" || password.value.trim() === ""){
+        alert("Всі поля повинні бути заповнені!");
+    }
+
+    const userData = {
+        email: email.value,
+        password: password.value
+    }
+
+    console.log(userData);
+
+    event.currentTarget.reset();
+});
+
 
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const buttonForChange = document.querySelector(".change-color");
+const colorText = document.querySelector(".color");
+const body = document.querySelector("body");
+
+
+buttonForChange.addEventListener("click", changeColor);
+
+function changeColor(){
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  colorText.textContent = randomColor;
+}
